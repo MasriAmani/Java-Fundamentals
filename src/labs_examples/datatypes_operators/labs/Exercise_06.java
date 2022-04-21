a5500169ae7c846f4059b113b1d4e83195aee0bc
Mohamad <amanimasri92@gmail.com>
2022-04-22 01:27:55 +0300

    Completed Datatypes_operators/Exercise_06

--- src/labs_examples/datatypes_operators/labs/Exercise_06.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_06.java
@@ -9,10 +9,22 @@ package labs_examples.datatypes_operators.labs;
  */
 
 public class Exercise_06 {
-
+    static float pi = 3.14f;
     public static void main(String[] args) {
 
-        // write code here
+      float radius = 3.14f;
+      float height = 5;
+      System.out.println("Volume is equal to: " + cylVolume(radius,height));
+      System.out.println("Surface area is equal to: "+ cylArea(radius,height));
 
     }
+
+    public static float cylVolume (float radius,float height){
+           return pi * radius * radius * height;
+    }
+
+    public static float cylArea(float radius,float height){
+        return 2 * pi * radius *(radius + height);
+    }
+
 }
