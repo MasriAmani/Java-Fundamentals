dd4ebdbada741975de1a228fc526f683fb027e06
Mohamad <amanimasri92@gmail.com>
2022-04-23 05:03:12 +0300

    Completed conditions_loops/Exercise_02

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 2: Days of the week
  *
@@ -16,9 +18,83 @@ public class Exercise_02 {
     public static void main(String[] args) {
 
         // 1) create scanner (don't forget to import Scanner!)
+        Scanner scanner = new Scanner(System.in);
+
         // 2) prompt user
+        System.out.print("Enter a number corresponding to the week day:");
+
         // 3) assign input to variable as int
+        int number = scanner.nextInt();
+
         // 4) write completed code here
 
+        //using if-else
+
+        if (number == 1)
+
+            System.out.println("Monday");
+
+        else if (number == 2)
+
+            System.out.println("Tuesday");
+
+        else if (number == 3)
+
+            System.out.println("Wednesday");
+
+        else if (number == 4)
+
+            System.out.println("Thursday");
+
+        else if (number == 5)
+
+            System.out.println("Friday");
+
+        else if (number == 6)
+
+            System.out.println("Saturday");
+
+        else if (number == 7)
+
+            System.out.println("Sunday");
+
+        else
+            System.out.println("Other");
+        
+
+        // using nested-if
+
+        if (number <= 7){
+            if(number <= 6){
+                if(number <= 5){
+                    if (number <= 4){
+                        if (number <= 3){
+                            if (number <= 2){
+                                if (number <= 1){
+                                    System.out.println("Monday");
+                                    return;
+                                }
+                                System.out.println("Tuesday");
+                                return;
+                            }
+                            System.out.println("Wednesday");
+                            return;
+                        }
+                        System.out.println("Thursday");
+                        return;
+                    }
+                    System.out.println("Friday");
+                    return;
+                }
+                System.out.println("Saturday");
+                return;
+            }
+            System.out.println("Sunday");
+            return;
+        }
+
+        else
+
+            System.out.println("Other");
     }
 }
