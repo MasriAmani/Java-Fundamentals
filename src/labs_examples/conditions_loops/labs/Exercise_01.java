bbe1a38cac5c2a9d155279e1d728b491f8bd585f
Mohamad <amanimasri92@gmail.com>
2022-04-23 05:02:46 +0300

    Completed conditions_loops/Exercise_01

--- src/labs_examples/conditions_loops/labs/Exercise_01.java
+++ src/labs_examples/conditions_loops/labs/Exercise_01.java
@@ -23,7 +23,12 @@ public class Exercise_01 {
         // assign input to variable as int
         int number = scanner.nextInt();
 
-        // write completed code here
+        if (number % 2 == 0)
+            System.out.println("The number you entered is even");
+
+        else
+            System.out.println("The number you entered is odd");
+
 
     }
 }
