da2462b80db034cf6c5a27db5b2d9e66bed545a9
Mohamad <amanimasri92@gmail.com>
2022-04-24 22:12:04 +0300

    Completed conditions_loops/Exercise_10

--- src/labs_examples/conditions_loops/labs/Exercise_10.java
+++ src/labs_examples/conditions_loops/labs/Exercise_10.java
@@ -8,4 +8,15 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_10 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10 ; i++){
+
+            if (i % 2 == 0)
+                continue;            // If i is even it will skip to the next iteration
+
+            System.out.println(i);   // Only odd numbers will be printed
+        }
+    }
 }
