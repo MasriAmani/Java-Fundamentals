a39b67db9e72b2970ca3cf511ac6575ae5743c29
Mohamad <amanimasri92@gmail.com>
2022-05-06 23:04:11 +0300

    Completed Arrays/Exercise_07

--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -1,5 +1,8 @@
 package labs_examples.arrays.labs;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
 /**
  *  ArrayLists
  *
@@ -10,4 +13,34 @@ package labs_examples.arrays.labs;
  *
  */
 public class Exercise_07 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(9);
+        list.add(4);
+
+        System.out.println("First element in arraylist is "+list.get(0));
+        System.out.println("The index of the number 9 in the arraylist is: "+list.indexOf(9));
+        System.out.println("The size of the arraylist is: "+list.size());
+
+        // adding element 10 at index 3
+        list.add(3,10);
+        System.out.println("Element at index 3 now is: "+list.get(3));
+        // size would increase by 1
+        System.out.println("The size of the arraylist now is: "+list.size());
+
+        //check if the list is empty
+        System.out.println(list.isEmpty());
+
+        // remove index at 0
+        list.remove(0);
+        System.out.println("Element at index 0 now is: "+list.get(0));
+        // size will be reduced by 1
+        System.out.println("The size of the arraylist now is: "+list.size());
+    }
+
+
 }
