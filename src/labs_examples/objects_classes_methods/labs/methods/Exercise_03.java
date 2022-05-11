41aea66722527a593ff82fb0e4dce6e8d2a64fd7
Mohamad <amanimasri92@gmail.com>
2022-05-12 01:07:36 +0300

    Completed Methods/Exercise_03

--- src/labs_examples/objects_classes_methods/labs/methods/Exercise_03.java
+++ src/labs_examples/objects_classes_methods/labs/methods/Exercise_03.java
@@ -1,5 +1,6 @@
 package labs_examples.objects_classes_methods.labs.methods;
 
+
 /**
  * Create a recursive method named factorial that will return the factorial of any number passed to it.
  *
@@ -10,9 +11,16 @@ package labs_examples.objects_classes_methods.labs.methods;
 public class Exercise_03 {
 
     public static void main(String[] args) {
-//        int x = factorial(5);
-//        System.out.println(x);
+      int x = factorial(5);
+       System.out.println(x);
 //        should print 120
+
+    }
+
+    public static int factorial (int a){
+        if (a == 0 | a == 1 )
+            return 1;
+        return a * factorial(a-1);
     }
 
 
