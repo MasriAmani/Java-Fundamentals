40d784e04fda4d3dd9e02d67e990ed889855c4da
Mohamad <amanimasri92@gmail.com>
2022-05-12 01:01:12 +0300

    Created Person Class

---
+++ src/labs_examples/objects_classes_methods/labs/methods/Person.java
@@ -0,0 +1,13 @@
+package labs_examples.objects_classes_methods.labs.methods;
+
+public class Person {
+
+    String name;
+    int age;
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+}
