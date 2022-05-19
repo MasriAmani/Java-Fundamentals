8348ec08ea3c69ef5dbbaae61df75006319164d3
Mohamad <amanimasri92@gmail.com>
2022-05-20 00:19:58 +0300

    Completed OOP/A_inheritance/Exercise_02

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
@@ -3,8 +3,10 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 /**
  * Run the class below and study the output. What prints and why?
  *
- * Your answer:
- *
+ * Your answer: It prints 10
+ * Because in the main method the object "a" created of type subclass "B" is referenced using superclass reference
+ * so it will have access only to those parts(methods and variables) of the object defined by the superclass "A"
+ * so it can only access the variable "i" in the superclass "A", which has the value 10
  */
 class A {
     int i = 10;
